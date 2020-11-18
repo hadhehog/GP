12fe4f816257cdf98946aba6e69e28025b5b359a
Mona Lisa <52933770+hadhehog@users.noreply.github.com>
2020-11-18 11:41:32 +0100

    Movement

---
+++ test/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    private Animator m_Anim;
+
+    private bool m_Running = false;
+
+    private NavMeshAgent m_Agent;
+
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                m_Agent.destination = hit.point;
+            }
+        }
+        if(m_Agent.remainingDistance <= m_Agent.stoppingDistance)
+        {
+            m_Running = false;
+        }
+        else
+        {
+            m_Running = true;
+        }
+
+        m_Anim.SetBool("running", m_Running);
+
+
+      /*{
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+                m_Agent.destination = m_HitInfo.point;
+        }*/
+    }
+}
---
+++ test/Assets/Smooth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smooth : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 10.0f;
+    public float height = 5.0f;
+    public float heightDamping = 2.0f;
+    public float rotationDamping = 3.0f;
+
+    void LateUpdate()
+    {
+        if (!target) return;
+
+        float wantedHeight = target.position.y + height;
+
+        float currentHeight = transform.position.y;
+
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+        transform.position = target.position;
+        transform.position -= Vector3.forward * distance;
+
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        transform.LookAt(target);
+    }
+}
+
+
+
