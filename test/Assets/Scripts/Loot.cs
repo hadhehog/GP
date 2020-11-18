f2a84673b54c34de9e92f252b105bea880f8ded6
Mona Lisa <52933770+hadhehog@users.noreply.github.com>
2020-11-18 13:56:29 +0100

    Loot Script

---
+++ test/Assets/Scripts/Loot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Loot
+{
+    //public Item item;
+    public float chance;
+    public int minQuantity;
+    public int maxQuantity;
+
+    public int Quantity { get; set; }
+}
+
+[CreateAssetMenu]
+public class LootTable: ScriptableObject
+{
+    public Loot[] loots;
+
+    public ArrayList GetLoot()
+    {
+        ArrayList dropList = new ArrayList();
+        float drawn = Random.Range(0f, 100f);
+
+        foreach (Loot loot in loots)
+        {
+            if (drawn <= loot.chance)
+            {
+                loot.Quantity = RandomQuantity(loot);
+                dropList.Add(loot);
+            }
+        }
+
+        return dropList;
+    }
+
+    public int RandomQuantity(Loot loot)
+    {
+        return Random.Range(loot.minQuantity, loot.maxQuantity);
+    }
+}
